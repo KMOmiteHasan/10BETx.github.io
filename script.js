27823d784ba9b69884882d9d8b78a420b54f413e
KMOmiteHasan <126360771+KMOmiteHasan@users.noreply.github.com>
2023-03-19 11:16:21 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 let navbarSupportedContent = document.getElementById("navbarSupportedContent")
 let navbarToggler = document.querySelector(".navbar-toggler")
 const navbar = document.querySelector(".navbar");
-
+const stickyNavbar = document.querySelector(".sticky-navbar")
+const show = document.querySelector(".show-nav")
 
 if (window.innerWidth <= 990) {
     navbarToggler.addEventListener("click", () => {
         if (navbarSupportedContent.classList.contains("hide")) {
             navbarSupportedContent.classList.remove("hide")
-            navbarSupportedContent.classList.add("show")
+            navbarSupportedContent.classList.add("show-nav")
+            navbar.classList.add("sticky-navbar-height")
         } else {
             navbarSupportedContent.classList.add("hide")
-            navbarSupportedContent.classList.remove("show")
+            navbarSupportedContent.classList.remove("show-nav")
+            navbar.classList.remove("sticky-navbar-height")
         }
     })
     navbar.classList.remove("sticky-navbar");
-
 }
 else {
     navbarSupportedContent.classList.add("hide")
-    navbarSupportedContent.classList.remove("show")
+    navbarSupportedContent.classList.remove("show-nav")
     navbar.classList.add("sticky-navbar");
 }
 window.addEventListener('scroll', () => {
