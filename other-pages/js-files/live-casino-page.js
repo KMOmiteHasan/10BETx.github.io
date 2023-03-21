d66294ae3b79105b07adfa2843d36295b1b8bd86
KMOmiteHasan <126360771+KMOmiteHasan@users.noreply.github.com>
2023-03-22 04:12:56 +0600

    Add files via upload

--- other-pages/js-files/live-casino-page.js
+++ other-pages/js-files/live-casino-page.js
@@ -1,6 +1,5 @@
 const filterItems = Array.from(document.querySelectorAll(".filter-item"))
 
-
 filterItems.forEach(item => {
     item.addEventListener('click', event => {
         event.preventDefault()
@@ -10,3 +9,39 @@ filterItems.forEach(item => {
         item.classList.add('active')
     })
 })
+
+let navbarSupportedContent = document.getElementById("navbarSupportedContent")
+let navbarToggler = document.querySelector(".navbar-toggler")
+const navbar = document.querySelector(".navbar");
+const stickyNavbar = document.querySelector(".sticky-navbar")
+const show = document.querySelector(".show-nav")
+
+if (window.innerWidth <= 990) {
+    navbarToggler.addEventListener("click", () => {
+        if (navbarSupportedContent.classList.contains("hide")) {
+            navbarSupportedContent.classList.remove("hide")
+            navbarSupportedContent.classList.add("show-nav")
+            navbar.classList.add("sticky-navbar-height")
+        } else {
+            navbarSupportedContent.classList.add("hide")
+            navbarSupportedContent.classList.remove("show-nav")
+            navbar.classList.remove("sticky-navbar-height")
+        }
+    })
+    navbar.classList.remove("sticky-navbar");
+}
+else {
+    navbarSupportedContent.classList.add("hide")
+    navbarSupportedContent.classList.remove("show-nav")
+    navbar.classList.add("sticky-navbar");
+}
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+
+    if (scrollHeight > navHeight) {
+        navbar.classList.add("sticky-navbar");
+    } else {
+        navbar.classList.remove("sticky-navbar");
+    }
+});
